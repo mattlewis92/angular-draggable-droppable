f6ed3f545d1702f206cc9db5dd639c68ce969a90
Matt Lewis <matt@clickup.com>
2025-08-23 18:32:30 +0100

    build: upgrade to angular 17

--- projects/angular-draggable-droppable/src/test.ts
+++ projects/angular-draggable-droppable/src/test.ts
@@ -1,12 +1,8 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js';
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/proxy';
-import 'zone.js/dist/mocha-patch';
+import 'zone.js/testing';
+import 'zone.js/plugins/mocha-patch';
 import './test-polyfills';
 import { getTestBed } from '@angular/core/testing';
 import {
